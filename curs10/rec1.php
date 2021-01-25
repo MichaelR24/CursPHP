58c6b6df67993f08124a6061b5bdb5ec02b74c9e
Adrian Adiaconitei <adrian@hipo.ro>
2021-01-25 21:44:36 +0200

    curs10

---
+++ curs10/rec1.php
@@ -0,0 +1,18 @@
+<?php
+// https://www.w3resource.com/php-exercises/
+// Calculul si afisarea sumei cifrelor unui numar natural n.
+// Date de intrare numar Ex. $n = 123454321
+// Date  de iesire  suma = 1+2+3+4+5+4+3+2+1 = 25
+declare(strict_types=1);
+function sumaCifre( int $n):int{
+    $s=0; //initializarea sumei cu 0 
+    while($n>0) { 
+        $ultimaCifra = $n % 10; // determina ultima cifra a numarului
+        $s=$s + $ultimaCifra; //adaugarea ultimei cifre la suma 
+        $n= (int) ($n / 10); // taierea ultimei cifre a variabilei n 
+    } 
+    return $s; // returneaza sumei
+}
+
+$suma = sumaCifre(123454321);
+echo $suma;
