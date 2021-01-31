8a522939e4bdcd0d58016df2fba599629124e9c2
Adrian Adiaconitei <adrian@hipo.ro>
2021-01-31 23:30:49 +0200

    curs 11

--- curs10/Carti.php
+++ curs10/Carti.php
@@ -1,68 +1,18 @@
 <?php
-// facem o schita / template/ sablon   pentru viitoarele obiecte de tip clasa : Membrii proprietati si metoade
 
-class Carti
-{
+class Carti{
+    public $titlu = 'Demo';
+    public $autor = 'Mihai';
 
-    //atribute sau proprietatile clasei
-    public $titlu;
-    public $autor;
-    static $editura;
-    public $an_aparitie;
-    public $nr_pagini;
-    public $pret;
-
-    // metodele  care seteaza atributele  de regula au parametru/ parametrii (argumente)
-
-    public function setTitlu($titlu_carte)
-    {
-        $this->titlu = $titlu_carte;
+    public function info (){
+        echo " Informatii despre carte in curand!";
     }
-    public function setAutor($autor_carte)
-    {
-        $this->autor = $autor_carte;
-    }
-    public function setEditura($editura_carte)
-    {
-        $this->editura = $editura_carte;
-    }
-    public function setAn($an_publicare)
-    {
-        $this->an_aparitie = $an_publicare;
-    }
-    public function setNrPagini($pagini)
-    {
-        $this->nr_pagini = $pagini;
-    }
-    public function setPret($pret_carte)
-    {
-        $this->pret = $pret_carte;
-    }
-
-    //metoadele care ofera informatii despre valoarea atributelor, de regula nu au parametrii
-    public function getTitlu()
-    {
-        return $this->titlu;
-    }
-    public function getAutor()
-    {
-        return $this->autor;
-    }
-    public function getEditura()
-    {
-        return $this->editura;
-    }
-    public function getAn()
-    {
-        return $this->an_aparitie;
-    }
-    public function getPagini()
-    {
-        return $this->nr_pagini;
-    }
-    public function getPret()
-    {
-        return $this->pret;
-    }
-
 }
+
+$carte = new  Carti();
+$carte2 = new Carti;
+
+echo "<pre>";
+var_dump($carte);
+echo "<br>";
+var_dump($carte2);
--- curs10/rec1.php
+++ curs10/rec1.php
@@ -1,10 +1,18 @@
 <?php
 // https://www.w3resource.com/php-exercises/
-// Calculul si afisarea sumei cifrelor unui numar natural n.
+// Calculul si afisarea sumei cifrelor unui numar 
+//natural n.
 // Date de intrare numar Ex. $n = 123454321
 // Date  de iesire  suma = 1+2+3+4+5+4+3+2+1 = 25
+
+
+
+
+
+
 declare(strict_types=1);
 function sumaCifre( int $n):int{
+    ($n<0)?$n = $n* (-1): $n = $n;
     $s=0; //initializarea sumei cu 0 
     while($n>0) { 
         $ultimaCifra = $n % 10; // determina ultima cifra a numarului
@@ -14,5 +22,5 @@ function sumaCifre( int $n):int{
     return $s; // returneaza sumei
 }
 
-$suma = sumaCifre(123454321);
+$suma = sumaCifre(-12);
 echo $suma;
